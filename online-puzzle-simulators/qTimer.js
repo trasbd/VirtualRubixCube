2083f1da6982549a4525ade185b25f75a03311e0
vxPhearingxv <rlpowers13@gmail.com>
2021-05-07 12:06:15 -0500

    Making sure all files are properly saved before turning project.

---
+++ online-puzzle-simulators/qTimer.js
@@ -0,0 +1,14 @@
+Qualtrics.SurveyEngine.addOnload(function () {
+	/*Place your JavaScript here to run when the page loads*/
+
+});
+
+Qualtrics.SurveyEngine.addOnReady(function () {
+	/*Place your JavaScript here to run when the page is fully displayed*/
+
+});
+
+Qualtrics.SurveyEngine.addOnUnload(function () {
+	/*Place your JavaScript here to run when the page is unloaded*/
+
+});
